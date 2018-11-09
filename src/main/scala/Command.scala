947c272d0b5e050737aef1d218709475d3f59d71
Shriharsh Mishra <harsh82@gmail.com>
2018-11-09 15:22:54 +0000

    Fixed Label and IfGoto command test cases fixed

--- src/main/scala/Command.scala
+++ src/main/scala/Command.scala
@@ -70,6 +70,8 @@ object Command {
     else if (List("and", "or").contains(name)) new BooleanCommand(command)
     else if (List("gt", "lt", "eq").contains(name)) new LogicalCommand(command)
     else if ("label" == name) new LabelCommand(command)
+    else if ("if-goto" == name) new IfGotoCommand(command)
+    else if ("goto" == name) new IfGotoCommand(command)
     else new OneOpCommand(command)
   }
 }
@@ -257,7 +259,7 @@ class IfGotoCommand(val fullCommand: String) extends Command {
 
   def readFromStack = List(stackPointer, "A=M", "D=M")
   def loadLabelAddress = List(s"@${parts(1)}")
-  def jumpIfGTZ = List("D; JGT")
+  def jumpIfGTZ = List("D;JGT")
 
   override def convert: List[String] = "//"+ fullCommand :: readFromStack ::: loadLabelAddress ::: jumpIfGTZ
 }
--- src/test/scala/CommandSpec.scala
+++ src/test/scala/CommandSpec.scala
@@ -10,11 +10,11 @@ class CommandSpec extends FlatSpec {
 //  }
 
   "Command" should " create a new LabelCommand when VM code has a label" in {
-    assert(Command("filename", "lable LOOP_START").isInstanceOf[LabelCommand])
+    assert(Command("filename", "label LOOP_START").isInstanceOf[LabelCommand])
   }
 
   "LabelCommand" should " convert when VM code to assembly" in {
-    assert(Command("filename", "lable LOOP_START").convert === List("(LOOP_START)"))
+    assert(Command("filename", "label LOOP_START").convert === List("// label LOOP_START", "(LOOP_START)"))
   }
 
   "Command" should " create a new IfGotCommand when VM code has a label" in {
@@ -22,6 +22,7 @@ class CommandSpec extends FlatSpec {
   }
 
   "IfGotoCommand" should " convert when VM code to assembly" in {
-    assert(Command("filename", "if-goto LOOP_START").convert === List("@SP", "A=M", "D=M", "@LOOP_START", "D;JGT"))
+    assert(Command("filename", "if-goto LOOP_START").convert ===
+      List("//if-goto LOOP_START","@SP", "A=M", "D=M", "@LOOP_START", "D;JGT"))
   }
 }
